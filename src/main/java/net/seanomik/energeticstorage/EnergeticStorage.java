45a3d5f2ed8744cae6f2e8639641e39a592bcb4b
TKronix <TKronix.mlg@gmail.com>
2021-12-11 05:06:32 +0000

    Added all the recipes from the plugin to the in-game player crafting guide on join

--- src/main/java/net/seanomik/energeticstorage/EnergeticStorage.java
+++ src/main/java/net/seanomik/energeticstorage/EnergeticStorage.java
@@ -1,5 +1,6 @@
 package net.seanomik.energeticstorage;
 
+import net.minecraft.server.v1_14_R1.Recipes;
 import net.seanomik.energeticstorage.commands.ESGiveCommand;
 import net.seanomik.energeticstorage.commands.ESReloadCommand;
 import net.seanomik.energeticstorage.files.ConfigFile;
@@ -11,11 +12,16 @@ import net.seanomik.energeticstorage.objects.ESSystem;
 import net.seanomik.energeticstorage.tasks.HopperTask;
 import net.seanomik.energeticstorage.utils.ItemRecipes;
 import net.seanomik.energeticstorage.utils.Reference;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.world.WorldSaveEvent;
+import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.xml.stream.events.Namespace;
 import java.util.*;
 
 public final class EnergeticStorage extends JavaPlugin implements Listener {
@@ -77,10 +83,37 @@ public final class EnergeticStorage extends JavaPlugin implements Listener {
         savePlayerSystems();
     }
 
+    // Adds all the custom recipes to the players crafting guide on join
     @EventHandler
-    public void onPlayerJoin()
+    public void onPlayerJoin(PlayerJoinEvent join)
     {
-
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "silicon"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "basic_processor"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "improved_processor"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "advanced_processor"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "quartz_enriched_iron"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "processor_binding"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "raw_basic_processor"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "raw_improved_processor"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "raw_advanced_processor"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "machine_casing"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "1k_storage_part"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "4k_storage_part"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "16k_storage_part"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "64k_storage_part"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "256k_storage_part"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "1024k_storage_part"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "4096k_storage_part"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "16384k_storage_part"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "es_system"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "es_drive_1k"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "es_drive_4k"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "es_drive_16k"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "es_drive_64k"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "es_drive_256k"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "es_drive_1024k"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "es_drive_4096k"));
+        join.getPlayer().discoverRecipe(new NamespacedKey(EnergeticStorage.getPlugin(), "es_drive_16384k"));
     }
 
     @Override
